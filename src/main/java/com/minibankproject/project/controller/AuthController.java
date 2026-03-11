5c9bd6a73073c38a5b37342987fd38a0f01df6f8
Thirubalan <thirubalan7@gmail.com>
2026-03-11 20:40:09 +0530

    commit

--- src/main/java/com/minibankproject/project/controller/AuthController.java
+++ src/main/java/com/minibankproject/project/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.minibankproject.project.controller;
 
 import com.minibankproject.project.dto.LoginRequest;
 import com.minibankproject.project.service.AuthService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,15 +19,24 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody  LoginRequest request)
-    {
+    public ResponseEntity<?> login(@RequestBody LoginRequest request, HttpSession session) {
         try {
+
             String token = authService.login(request);
-            return ResponseEntity.ok(Map.of("token", token));
+
+            // store data in session
+            session.setAttribute("userEmail", request.getEmail());
+
+            return ResponseEntity.ok(Map.of(
+                    "token", token,
+                    "sessionId", session.getId()
+            ));
+
         } catch (AuthenticationException ex) {
+
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(Map.of("error", "Invalid email or password"));
+
         }
     }
-
 }
