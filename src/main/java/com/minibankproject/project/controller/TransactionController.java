d17e08a71014cd83f70f5f9384d0ee69e0432127
Thirubalan <thirubalan7@gmail.com>
2026-03-11 21:06:39 +0530

    commit

--- src/main/java/com/minibankproject/project/controller/TransactionController.java
+++ src/main/java/com/minibankproject/project/controller/TransactionController.java
@@ -2,6 +2,7 @@ package com.minibankproject.project.controller;
 
 import com.minibankproject.project.entity.TransactionEntity;
 import com.minibankproject.project.service.TransactionService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,6 +17,14 @@ public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
+
+    @GetMapping("/profile")
+    public String profile(HttpSession session) {
+
+        String email = (String) session.getAttribute("userEmail");
+
+        return "Logged user: " + email;
+    }
 
     @PreAuthorize("hasAnyRole('EMPLOYEE','MANAGER')")
     @GetMapping("/account/{accountId}")
